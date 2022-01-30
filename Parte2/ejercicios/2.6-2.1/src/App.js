cd18214f32d44395cf38dc6154e3dd7c4e072ad9
Alejandro Widomlanski <widomlanski@hotmail.com>
2022-01-30 13:34:16 -0300

    ej-2.8

--- Parte2/ejercicios/2.6-2.1/src/App.js
+++ Parte2/ejercicios/2.6-2.1/src/App.js
@@ -46,7 +46,7 @@ export default App; */
 
 
 //EJ2.7
-import React, { useState } from 'react'
+/* import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -57,19 +57,21 @@ const App = () => {
 
   const handleAddPersons = (e) => {
     e.preventDefault()
-    const arrayName = persons.map((e)=> {
-     return e.name
+    const arrayName = persons.map((e) => {
+      return e.name
     })
 
-    if(arrayName.indexOf(newName) !== -1) {
+    if (arrayName.indexOf(newName) !== -1) {
       alert(`${newName} is already added to phonebook`)
       setNewName('')
     } else {
       const newPerson = {
         name: newName
       }
+      console.log(persons)
       setPersons([...persons, newPerson])
       setNewName('')
+      console.log(persons)
     }
   }
 
@@ -77,6 +79,7 @@ const App = () => {
     setNewName(e.target.value)
   }
 
+  console.log(persons)
 
   return (
     <div>
@@ -90,9 +93,94 @@ const App = () => {
         </div>
       </form>
       <ul>
-      {persons.map((e)=> {
-       return <li key={e.name}>{e.name}</li>
-      })}
+        {persons.map((e) => {
+          return <li key={e.name}>{e.name}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
+ */
+
+
+//EJ2.8
+import React, { useState } from 'react'
+
+const App = () => {
+
+  /*Datos de Agenda*/
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '1167138894' }
+  ])
+
+  /*Datos input Name*/
+  const [newName, setNewName] = useState('')
+
+
+  /*Datos input Number*/
+  const [newNumber, setNewNumber] = useState('')
+
+
+  /*Funciòn para agregar datos a la agenda*/
+  const handleAddPersons = (e) => {
+    e.preventDefault()
+    const arrayName = persons.map((e) => {
+      return e.name
+    })
+
+    const arrayNumber = persons.map((e) => {
+      return e.number
+    })
+
+    if (arrayName.indexOf(newName) !== -1) {
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      setNewNumber('')
+    } else if (arrayNumber.indexOf(newNumber) !== -1) {
+      alert(`${newNumber} is already added to phonebook`)
+      setNewName('')
+      setNewNumber('')
+    }
+    else {
+      const newPerson = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons([...persons, newPerson])
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  /*Funciòn que ve cambios en el input name*/
+  const handleChangeName = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const handleChangeNumber = (e) => {
+    setNewNumber(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPersons}>
+        <div>
+          name: <input value={newName} onChange={handleChangeName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleChangeNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <ul>
+        {persons.map((e) => {
+          return <li key={e.name}>{e.name} - {e.number}</li>
+        })}
       </ul>
     </div>
   )
