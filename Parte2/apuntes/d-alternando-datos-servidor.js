9a72200233074d1365fe71a8ebfc630a0dc9cf74
Alejandro Widomlanski <widomlanski@hotmail.com>
2022-02-06 15:36:14 -0300

    alterando-datos-servidor

---
+++ Parte2/apuntes/d-alternando-datos-servidor.js
@@ -0,0 +1,5 @@
+//REST
+
+/* En terminología REST, nos referimos a objetos de datos individuales, como las notas en nuestra aplicación, como recursos. 
+Cada recurso tiene una dirección única asociada: su URL.  */
+
--- Parte2/ejercicios/2.15-2.18/src/App.js
+++ Parte2/ejercicios/2.15-2.18/src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Note from "./components/Note"
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+
+const App = () => {
+
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
+  const [showAll, setShowAll] = useState(true)
+
+
+
+
+  const toggleImportance = (id) => () => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(
+        note => note.id !== id ? note : response.data))
+    })
+  }
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/notes')
+      .then((resultado) => {
+        setNotes(resultado.data)
+      })
+  }, []);
+
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date(),
+    }
+    setNewNote('')
+
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+      })
+  }
+
+
+  if (showAll) {
+    return (
+      <div>
+        <h1>Notes</h1>
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all'}
+          </button>
+        </div>
+        <ul>
+          {notes.map(note => (
+            <Note key={note.id} note={note} toggleImportance={toggleImportance(note.id)} />
+          ))}
+        </ul>
+        <form onSubmit={addNote} >
+          <input
+            value={newNote}
+            onChange={handleNoteChange} />
+          <button type="submit">save</button>
+        </form>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>Notes</h1>
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all'}
+          </button>
+        </div>
+        <ul>
+          {notes.filter(note => note.important === true).map((note => (
+            <Note key={note.id} note={note} toggleImportance={toggleImportance(note.id)} />
+          )))}
+        </ul>
+        <form onSubmit={addNote} >
+          <input
+            value={newNote}
+            onChange={handleNoteChange} />
+          <button type="submit">save</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
